5187d4985aaf14f2e2ed6d17394968a3613c7e1c
mrobota <mrobota@livegenic.com>
2020-10-09 01:36:40 +0300

    identity service Refactoring

---
+++ identity-service/app/views.py
@@ -0,0 +1,47 @@
+from .models import Users
+from app import app, db
+from flask import jsonify
+from flask import request
+
+
+# from flask import redirect
+
+@app.route('/')
+def index():
+    return 'identity-service is working!'
+
+
+@app.route('/user', methods=['POST'])
+def user():
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        user = Users(name=name, password=password)
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+        return 'Added new user'
+    except:
+        return 'Error'
+
+
+# Display All events from database
+@app.route('/users/', methods=['GET'])
+def users():
+    # data = cur.fetchall()
+    # data = jsonify({'users': list(map(lambda users: users.serialize(), Users.query.all()))})
+    # return render_template('index.html', data=data)
+    return jsonify({'users': list(map(lambda users: users.serialize(), Users.query.all()))})
+
+
+# # Route to Delete an event from the MySQL Database
+@app.route('/events/<int:id>', methods=['DELETE'])
+def delete(id):
+    try:
+        db.session.delete(Users.query.filter_by(id=id).first())
+        db.session.commit()
+        # return redirect('/users')
+        return 'Deleted user #' + id
+    except:
+        return 'Error'
