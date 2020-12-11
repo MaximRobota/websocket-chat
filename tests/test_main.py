ff7b42eb5b3b8947473c89a8daa7c29af45d6819
Maxim1106 <Maxim11061106@gmail.com>
2020-12-12 00:41:42 +0200

    Check tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,10 @@
-from time import sleep
+import os
 import pytest
 import requests
+import socketio
 import subprocess
-import os
 from pathlib import Path
+from time import sleep
 
 REPO_ROOT = Path(os.path.realpath(__file__)).parent.parent
 
@@ -31,6 +32,7 @@ def prepare_services():
 def register_user(email, password):
     return requests.post(
         'http://localhost:5051/auth/register', data={'email': email, 'password': password}).json()
+
 
 def login_user(email, password):
     return requests.post(
@@ -90,3 +92,39 @@ def test_user_status_fake_token():
     resp_status = requests.get('http://localhost:5051/auth/status', headers=headers)
     resp_status_json = resp_status.json()
     assert resp_status_json['status'] == 'fail'
+
+
+@pytest.mark.usefixtures('prepare_services')
+def test_connect_send_message_ws():
+    user_email = 'Vasiya@gm.com'
+    password = 'pass'
+    register_user(user_email, password)
+    resp_login = login_user(user_email, password)
+
+    # assert 'auth_token' in resp_login
+
+    headers = {'Authorization': 'Bearer ' + resp_login['auth_token']}
+    resp_status = requests.get('http://localhost:5051/auth/status', headers=headers)
+    resp_status_json = resp_status.json()
+    # assert resp_status_json['user']['email'] == user_email
+    # assert 'id' in resp_status_json['user']
+
+    sio = socketio.AsyncClient()
+
+    @sio.event
+    async def connect():
+        await sio.connect('http://localhost:5053')
+        await sio.wait()
+        print('my sid is')
+
+        if sio.connected:
+            await sio.emit('my message', ["join", {"room": "1"}])
+            assert 'id' != 'id'
+    # connect()
+    #
+    # @sio.event
+    # def my_event(sid, data):
+    #     # handle the message
+    #     return "OK", 123
+
+    # sio.disconnect()
